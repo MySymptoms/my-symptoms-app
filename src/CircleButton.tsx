e69e7784c1bac1885dbed54309c92dc9cd91d0fe
André Samuelsson <andre.samuelsson@navico.com>
2020-03-26 00:17:09 +0100

    Extract styles of Circular button and Header row to styled components

--- src/CircleButton.tsx
+++ src/CircleButton.tsx
@@ -1,17 +1,24 @@
 import React, {FC} from 'react';
 import styled from 'styled-components/native';
+import {TouchableOpacity} from 'react-native';
 
-interface Props {}
+interface Props {
+  onPress: () => {};
+}
 
-export const CircleButton: FC<Props> = ({children}) => {
-  return <Container>{children}</Container>;
+export const CircleButton: FC<Props> = ({onPress, children}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Container style={{alignSelf: 'baseline'}}>{children}</Container>
+    </TouchableOpacity>
+  );
 };
 
 const Container = styled.View`
-  border-radius: 100px;
-  width: 30px;
-  height: 30px;
+  border-radius: 200px;
   border-width: 2px;
+  width: 50px;
+  height: 50px;
   background-color: #333333;
   justify-content: center;
   align-items: center;
--- src/OverviewScreen.tsx
+++ src/OverviewScreen.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import styled from 'styled-components/native';
-import {Text, StyleSheet, View, Image} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {CircleButton} from './CircleButton';
-import Hedgehog from '../assets/hedgehog.png';
+import styled from 'styled-components/native';
 
 export const OverviewScreen = () => {
   return (
     <LinearGradient
       colors={['#000000', '#rgba(37,37,37,0)', '#2E2E2E']}
       style={styles.linearGradient}>
-      <View>
-        <CircleButton>
-          <Image source={Hedgehog} />
-        </CircleButton>
-        <Text style={{color: 'white'}}>TRACK MY COVID</Text>
-      </View>
+      <HeaderRow>
+        <View style={{flex: 1}}></View>
+        <CenterTitleText>TRACK MY SYMPTOMS</CenterTitleText>
+        <View style={{flex: 1, alignItems: 'flex-end'}}>
+          <CircleButton onPress={() => {}}>
+            <UserEmojiContainer>🦔</UserEmojiContainer>
+          </CircleButton>
+        </View>
+      </HeaderRow>
     </LinearGradient>
   );
 };
+
+const CenterTitleText = styled.Text`
+  flex: 2;
+  font-family: Dosis;
+  color: #c1c1c1;
+`;
+
+const UserEmojiContainer = styled.Text`
+  justify-content: center;
+  align-items: center;
+  line-height: 24px;
+  font-size: 24px;
+`;
+
+const HeaderRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 10px;
+`;
 
 const styles = StyleSheet.create({
   linearGradient: {
