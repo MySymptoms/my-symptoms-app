382581d894aaffefb384076810a9e292264be684
André Samuelsson <andre.samuelsson@navico.com>
2020-03-28 16:30:11 +0100

    Change No symptoms today button to new design to fit screen.

--- src/OverviewScreen.tsx
+++ src/OverviewScreen.tsx
@@ -101,6 +101,7 @@ export const OverviewScreen: FC<Props> = ({navigation}) => {
         <Space />
         <NoSymtoms>
           <Icon source={Icons.Flex} />
+          <Space />
           <Text style={styles.emojiButtonText}>No symptoms today</Text>
         </NoSymtoms>
       </View>
@@ -109,8 +110,9 @@ export const OverviewScreen: FC<Props> = ({navigation}) => {
 };
 
 const NoSymtoms = styled.View`
-  width: 110px;
-  height: 110px;
+  flex-direction: row;
+  width: 245px;
+  height: 62px;
   border: 2px solid black;
   justify-content: center;
   align-items: center;
