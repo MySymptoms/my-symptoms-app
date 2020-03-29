3604fcab95b569e46be1a99a5b56423ab993d324
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-03-30 01:21:49 +0200

    Remove double padding in summary page

--- src/SummaryPage.tsx
+++ src/SummaryPage.tsx
@@ -74,7 +74,6 @@ export const SummaryPage = () => {
   const userEmoji = useSelector(selectUserEmoji);
   return (
     <Background
-      style={{paddingHorizontal: 15}}
       header={
         <NavigationHeader
           showBackButton
@@ -83,61 +82,67 @@ export const SummaryPage = () => {
           onPressRight={() => navigation.navigate('DetailedReport')}
         />
       }>
-      <Row>
-        <SummaryViewColumn>
-          <MyText style={{fontSize: 36}}>Age</MyText>
-          <MyText style={{fontSize: 60, color: '#fff'}}>55</MyText>
-        </SummaryViewColumn>
+      <View style={{paddingHorizontal: 15}}>
+        <Row>
+          <SummaryViewColumn>
+            <MyText style={{fontSize: 36}}>Age</MyText>
+            <MyText style={{fontSize: 60, color: '#fff'}}>55</MyText>
+          </SummaryViewColumn>
+          <Space />
+          <SummaryViewColumn>
+            <MyText>Travelled</MyText>
+            <BlackBox
+              style={{borderRadius: 50, padding: 10, backgroundColor: '#000'}}>
+              <CheckIcon />
+            </BlackBox>
+          </SummaryViewColumn>
+        </Row>
         <Space />
-        <SummaryViewColumn>
-          <MyText>Travelled</MyText>
-          <BlackBox
-            style={{borderRadius: 50, padding: 10, backgroundColor: '#000'}}>
-            <CheckIcon />
-          </BlackBox>
-        </SummaryViewColumn>
-      </Row>
-      <Space />
-      <RowWithContentRight
-        leftText={'Fever'}
-        right={
-          <BlackBox
-            style={{flex: 2, backgroundColor: '#ffbc5c', alignItems: 'center'}}>
-            <MyText style={{fontSize: 60, color: '#000', padding: 14}}>
-              37.9
-            </MyText>
-          </BlackBox>
-        }
-      />
-      <Space />
-      <View>
         <RowWithContentRight
-          leftText={'Medical Conditions'}
+          leftText={'Fever'}
           right={
-            <View
+            <BlackBox
               style={{
-                flex: 1,
-                alignItems: 'space-between',
+                flex: 2,
+                backgroundColor: '#ffbc5c',
+                alignItems: 'center',
               }}>
-              <BlackBox
-                style={{
-                  borderRadius: 50,
-                  padding: 20,
-                  backgroundColor: '#000',
-                }}>
-                <CheckIcon height={50} width={50} />
-              </BlackBox>
-            </View>
+              <MyText style={{fontSize: 60, color: '#000', padding: 14}}>
+                37.9
+              </MyText>
+            </BlackBox>
           }
         />
-        <HalfSpace />
-        <SummaryViewListItem text="Asthma" />
-        <HalfSpace />
-        <SummaryViewListItem text="Heart conditions" />
-        <HalfSpace />
-        <SummaryViewListItem text="Immunocompromised" />
-        <HalfSpace />
-        <SummaryViewListItem text="Diabetes" />
+        <Space />
+        <View>
+          <RowWithContentRight
+            leftText={'Medical Conditions'}
+            right={
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'space-between',
+                }}>
+                <BlackBox
+                  style={{
+                    borderRadius: 50,
+                    padding: 20,
+                    backgroundColor: '#000',
+                  }}>
+                  <CheckIcon height={50} width={50} />
+                </BlackBox>
+              </View>
+            }
+          />
+          <HalfSpace />
+          <SummaryViewListItem text="Asthma" />
+          <HalfSpace />
+          <SummaryViewListItem text="Heart conditions" />
+          <HalfSpace />
+          <SummaryViewListItem text="Immunocompromised" />
+          <HalfSpace />
+          <SummaryViewListItem text="Diabetes" />
+        </View>
       </View>
     </Background>
   );
