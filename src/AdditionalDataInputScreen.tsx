24727cd19c459a0e8b380c3f1350f26d3fa86784
Anton Levholm <anton.badminton@gmail.com>
2020-03-29 14:02:01 +0200

    Add text and buttons to AdditionalDataInputScreen

--- src/AdditionalDataInputScreen.tsx
+++ src/AdditionalDataInputScreen.tsx
@@ -3,12 +3,12 @@ import {Background} from './components/Background';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {Icon, Icons} from './lib/icons';
 import {NavigationHeader} from './NavigationHeader';
-import {View, StyleSheet, Text, Image} from 'react-native';
-import {Colors} from './lib/colors';
+import {View, StyleSheet, Text, Image, Dimensions} from 'react-native';
 import {fontName} from './lib/vars';
 import styled from 'styled-components/native';
-import { SegmentedControl } from './components/SegmentedControl';
-import map from './images/map.png';
+import {SegmentedControl} from './components/SegmentedControl';
+import map3x from './images/map3x.png';
+import {Space} from './components/Block';
 
 type Props = {
   navigation: StackNavigationProp<{}>;
@@ -38,11 +38,12 @@ const BoxText = styled.Text`
 export const AdditionalDataInputScreen: FC<Props> = ({navigation}) => {
   const [hasTraveled, setHasTraveled] = useState(true);
   const [hasCondition, setHasCondition] = useState(false);
-  
+
   return (
     <Background>
       <NavigationHeader title={'HELLO ANONYMOUS'} showBackButton />
-      <BlackBox style={{...styles.contentMargin, marginTop: 20}}>
+      <Space />
+      <BlackBox>
         <Row style={styles.outerRow}>
           <Row>
             <Icon source={Icons.Baby} style={styles.emojiStyle} />
@@ -51,7 +52,8 @@ export const AdditionalDataInputScreen: FC<Props> = ({navigation}) => {
           <Text style={styles.year}>1984</Text>
         </Row>
       </BlackBox>
-      <BlackBox style={styles.contentMargin}>
+      <Space />
+      <BlackBox>
         <Row style={styles.outerRow}>
           <Row>
             <Icon source={Icons.PlaneLanding} style={styles.emojiStyle} />
@@ -67,7 +69,8 @@ export const AdditionalDataInputScreen: FC<Props> = ({navigation}) => {
           />
         </Row>
       </BlackBox>
-      <BlackBox style={styles.contentMargin}>
+      <Space />
+      <BlackBox>
         <Row style={styles.outerRow}>
           <Row>
             <Icon source={Icons.Pill} style={styles.emojiStyle} />
@@ -83,12 +86,74 @@ export const AdditionalDataInputScreen: FC<Props> = ({navigation}) => {
           />
         </Row>
       </BlackBox>
+      <Space />
+      <Space />
       <View style={styles.center}>
-        <Image source={map} style={styles.map} />
+        <Button>
+          <Row>
+            <Icon source={Icons.Corona} style={styles.emojiStyle} />
+            <ButtonText>I have been diagnosed with Covid-19</ButtonText>
+          </Row>
+        </Button>
+        <Space />
+        <Image
+          source={map3x}
+          width={Dimensions.get('window').width}
+          style={styles.map}
+        />
+        <ShareText>
+          If you are interested, you can help us compile a gloabal map that
+          illustrates what symptoms actually lead to Covid-19 💛
+        </ShareText>
+        <Space />
+        <ShareButton>
+          <ShareButtonText>Share data anonymously</ShareButtonText>
+        </ShareButton>
+        <Space />
+        <Row>
+          <View style={styles.peopleSharedBorder}>
+            <Text style={styles.peopleShared}>2234</Text>
+          </View>
+          <Text style={styles.text}>
+            {' '}
+            have shared their data anonymously so far
+          </Text>
+        </Row>
       </View>
     </Background>
   );
 };
+
+const Button = styled.TouchableOpacity`
+  align-items: center;
+  background: #333;
+  border-radius: 40px;
+  padding: 15px 20px;
+  border: 3px rgba(0, 0, 0, 0.6);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  elevation: 100;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+`;
+const ShareText = styled.Text`
+  color: white;
+  width: 90%;
+  line-height: 21px;
+  letter-spacing: 1px;
+`;
+
+const ShareButton = styled(Button)`
+  background: #008475;
+`;
+
+const ShareButtonText = styled.Text`
+  color: #003d02;
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 1px;
+`;
 
 const styles = StyleSheet.create({
   center: {
@@ -103,14 +168,23 @@ const styles = StyleSheet.create({
     color: 'white',
     alignSelf: 'flex-end',
   },
-  contentMargin: {
-    marginBottom: 20,
-  },
   outerRow: {
     justifyContent: 'space-between',
   },
   map: {
-    width: '70%',
+    width: Dimensions.get('window').width - 60,
+    height: (Dimensions.get('window').width - 60) / 1.75,
     resizeMode: 'contain',
+  },
+  text: {
+    color: 'white',
+  },
+  peopleShared: {
+    color: 'white',
+    fontWeight: '700',
+  },
+  peopleSharedBorder: {
+    borderBottomWidth: 2,
+    borderBottomColor: 'white',
   },
 });
