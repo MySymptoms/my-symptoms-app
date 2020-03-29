62c8033e8511998543c6c3a0e7ec539665afb40d
Anton Levholm <anton.badminton@gmail.com>
2020-03-29 22:00:57 +0200

    Add header as a prop

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,16 +1,21 @@
 import React, {FC} from 'react';
-import {StyleSheet, ViewStyle} from 'react-native';
+import {StyleSheet, ViewStyle, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {ScrollView} from 'react-native-gesture-handler';
 
-export const Background: FC<{style?: ViewStyle}> = ({children, style}) => (
+export const Background: FC<{style?: ViewStyle; header?: React.ReactNode}> = ({
+  children,
+  style,
+  header,
+}) => (
   <LinearGradient
     start={{x: 0.5, y: 1}}
     end={{x: 0.5, y: 0}}
     colors={['#000', 'rgba(37,37,37,0)']}
     locations={[0, 0.1502]}
     style={[styles.linearGradient, style]}>
-    <ScrollView style={styles.scrollView}>{children}</ScrollView>
+    {header && <View style={styles.header}>{header}</View>}
+    <ScrollView contentInset={styles.scrollView}>{children}</ScrollView>
   </LinearGradient>
 );
 
@@ -20,7 +25,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollView: {
+    top: 15,
+    bottom: 20,
+  },
+  header: {
     paddingLeft: 15,
     paddingRight: 15,
+    paddingBottom: 5,
   },
 });
