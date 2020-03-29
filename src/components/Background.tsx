31ecffd61dd68cd8f6d5b6de3b395cf3003b2e94
Anton Levholm <anton.badminton@gmail.com>
2020-03-29 14:01:21 +0200

    Put background children in ScrollView

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {StyleSheet, ViewStyle} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export const Background: FC<{style?: ViewStyle}> = ({children, style}) => (
   <LinearGradient
@@ -9,7 +10,7 @@ export const Background: FC<{style?: ViewStyle}> = ({children, style}) => (
     colors={['#000', 'rgba(37,37,37,0)']}
     locations={[0, 0.1502]}
     style={[styles.linearGradient, style]}>
-    {children}
+    <ScrollView>{children}</ScrollView>
   </LinearGradient>
 );
 
