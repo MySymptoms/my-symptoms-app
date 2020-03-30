8ffa983f3d7edf28bb60fe85860bb09b734d60b4
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-03-31 00:15:06 +0200

    Enable fancy error screen

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import 'react-native-get-random-values';
 import App from './App';
 import {name as appName} from './app.json';
+require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
