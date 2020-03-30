444e4be05324441289109c608f7a8eca28bdc164
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-03-30 13:52:14 +0200

    Cleaner splash screen

--- ios/MyCorona/AppDelegate.m
+++ ios/MyCorona/AppDelegate.m
@@ -35,6 +35,10 @@ static void InitializeFlipper(UIApplication *application) {
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  
+  UIView* launchScreenView = [[[NSBundle mainBundle] loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex:0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
