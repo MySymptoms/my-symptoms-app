6f57557530d001290711f318e061444178c946a6
André Samuelsson <andre.samuelsson@navico.com>
2020-04-02 22:31:52 +0200

    Import enable logbox first in index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * @format
  */
 
+import './before';
 import {AppRegistry} from 'react-native';
 import 'react-native-get-random-values';
 import App from './App';
 import {name as appName} from './app.json';
-require('react-native').unstable_enableLogBox();
 
 AppRegistry.registerComponent(appName, () => App);
